7c5408f02bc0f5f736a3b4cd57da48b884d28da1
RenydeSa <renykiil@gmail.com>
2024-05-16 21:31:34 -0300

    uso do link para navegar

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,26 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title> Curso Next.Js </title>
-      </Head>
-        
       <div className={styles.description}>
-        <h1> Curso NextJs </h1> 
+        <h1> Home</h1> 
+      </div>
+
+      <div className={styles.container}>
+        <ul>
+          <li>
+            <Link href="/products">
+              <h2> Produtos </h2>
+            </Link>
+
+          </li>
+        </ul>
       </div>
     </>
   );
