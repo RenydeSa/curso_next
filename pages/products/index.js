9c4904fb40219897eda6f692c8a1c684801da221
RenydeSa <renykiil@gmail.com>
2024-05-16 21:54:20 -0300

    adição de links

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,7 +1,37 @@
+import Link from "next/link";
 export default function Products(){
     return(
-        <h1> Página de Produtos </h1>
+        <>
+            <h1> Página de Produtos </h1>
 
+            <div>
+                <ul>
+                    <li>
+                        <Link href="/products/shirt">
+                            <h3> Camisa </h3>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/products/pants/redpants">
+                            <h3> Calça Vermelha </h3>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/products/pants/bluepants">
+                            <h3> Calça Azul </h3>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <button>
+                    <Link href="/">
+                        Voltar para Home
+                    </Link>
+                </button>
+            </div>
+        </>
         
     );
 
